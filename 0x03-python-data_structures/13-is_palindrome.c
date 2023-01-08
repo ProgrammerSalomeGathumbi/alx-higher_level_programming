7c279e162294a8e314bd4676f2a106c1a28446e5
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-01-08 16:54:35 +0300

    13 test

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,44 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
 /**
- *is_palindrome -checks if a singly linked list is a palindrome
- * @head: A pointer
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int length = 0;
-	int i;
-	int *values;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	/* Get the length of the list and store the values in an array */
-	while (current != NULL)
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	tmp = *head;
+	while (tmp)
 	{
-		length++;
-		current = current->next;
+		size++;
+		tmp = tmp->next;
 	}
-	values = malloc(sizeof(int) * length);
-	if (values == NULL)
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
 		return (0);
-	current = *head;
-	for (i = 0; i < length; i++)
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		values[i] = current->n;
-		current = current->next;
-	}
-	/* Compare the values in the array to the values in the list */
-	current = *head;
-	for (i = 0; i < length; i++)
-	{
-		if (values[i] != current->n)
-		{
-			free(values);
+		if (tmp->n != rev->n)
 			return (0);
-		}
-		current = current->next;
+		tmp = tmp->next;
+		rev = rev->next;
 	}
-	free(values);
+	reverse_listint(&mid);
+
 	return (1);
 }
