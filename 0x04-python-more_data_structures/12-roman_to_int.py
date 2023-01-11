e8f64250a4b0e43b714d4a229a11e9cd93bde367
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-01-11 20:11:00 +0300

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    roman_to_int = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    result = 0
+    for i in range(len(roman_string) - 1):
+        if roman_to_int[roman_string[i]] < roman_to_int[roman_string[i + 1]]:
+            result -= roman_to_int[roman_string[i]]
+        else:
+            result += roman_to_int[roman_string[i]]
+    return result + roman_to_int[roman_string[-1]]
