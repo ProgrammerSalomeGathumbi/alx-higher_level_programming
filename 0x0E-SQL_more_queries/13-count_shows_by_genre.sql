bdf8ff320f3a510d259af52d270bf31b195d5b32
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-03-15 17:30:00 +0300

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,5 @@
 -- Removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
-DELETE FROM second_table WHERE score <= 5
+SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
+FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
