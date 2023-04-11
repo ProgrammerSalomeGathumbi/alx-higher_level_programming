e15f276137984d952d2ce93f6970144a17384013
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-04-11 22:42:43 +0300

    Fixing errors in 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const y = i.length;
 if (y <= 3) {
   console.log(0);
 } else {
-  const x = i.map(Number).sort(function(a, b) {return b - a});
+  const x = i.map(Number).sort(function (a, b) { return b - a; });
   const y = x.slice(3);
   console.log(y[0]);
 }
