8c059b0031ea79687dd84cc6439ce237503b1aac
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-04-12 15:25:06 +0300

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    const i = parseInt(w);
+    const z = parseInt(h);
+    if (i > 0 && z > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const a = 'X';
+    let x;
+    let y;
+    for (x = 0; x < this.height; x++) {
+      for (y = 0; y < this.width; y++) {
+        process.stdout.write(a);
+      }
+      console.log('');
+    }
+  }
+
+  rotate () {
+    const b = this.width;
+    this.width = this.height;
+    this.height = b;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
