e6734179f8557c08469ce1f2758a9b601790cef6
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-04-15 18:51:43 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,11 +6,13 @@ N (upper N) from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+iif __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cursor.execute("SELECT * FROM states  
+		    WHERE name LIKE 'N%' 
+		    ORDERED BY id ASC")
     tables = cursor.fetchall()
     for table in tables:
         print(table)
