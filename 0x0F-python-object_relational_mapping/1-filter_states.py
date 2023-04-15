2b6ae058f7e7b308fad3443cf1b7de8a20311174
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-04-15 18:54:21 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,9 @@ iif __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states  
-		    WHERE name LIKE 'N%' 
-		    ORDERED BY id ASC")
+    cursor.execute("SELECT * FROM states
+                   WHERE name LIKE 'N%'
+                   ORDERED BY id ASC")
     tables = cursor.fetchall()
     for table in tables:
         print(table)
