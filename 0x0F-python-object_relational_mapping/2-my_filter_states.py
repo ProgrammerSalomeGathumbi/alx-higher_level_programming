2d33d8d2d7c60bec6f3fffbb997ccd223d5bcdbb
root <root@HP-PC>
2023-04-16 12:40:33 +0300

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,15 +5,15 @@ all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states\
-                   WHERE name LIKE '{:s}'\
-                   ORDER BY id ASC".format(argv[4]))
+                   WHERE name LIKE BINARY '{:s}'\
+                   ORDER BY states.id ASC".format(argv[4]))
     tables = cursor.fetchall()
     for table in tables:
         if table[1] == argv[4]:
