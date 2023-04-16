b672696ec9c41b62d0cf5527253ce759bf94b803
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-04-16 16:12:45 +0300

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A script that lists all states with a name starting with
-N (upper N) from the database hbtn_0e_0_usa
+A script that lists all cities from the database
+hbtn_0e_4_usa
 """
 import MySQLdb
 import sys
