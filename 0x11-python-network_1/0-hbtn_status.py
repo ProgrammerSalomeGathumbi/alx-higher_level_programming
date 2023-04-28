32c5c5ba416909b7f4c895bc8c42e8dadaeb6abf
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-04-28 13:07:03 +0300

    Added 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         data = response.read()
         print('Body response:')
-        print('\t - type:', type(data))
-        print('\t - content:', data)
-        print('\t -utf8 content:', data.decode('utf8'))
+        print('\t- type:', type(data))
+        print('\t- content:', data)
+        print('\t- utf8 content:', data.decode('utf8'))
