88f90f63935e477a15ee74f375e3e4b3a73e6b36
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-04-28 18:02:25 +0300

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub
+credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+    j = response.json()
+    try:
+        print(j.get('id'))
+    except Exception:
+        print('None')
