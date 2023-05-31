4d22b825324173f77fb2534fb8baf81f3a4d265e
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-05-31 22:19:28 +0300

    Added 8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    method: 'GET',
+    dataType: 'jsonp',
+    success: function (data) {
+      const movie = data.result;
+      for (let m = 0; m < movie.length; m++) {
+        $('#list_movies').append('<li>'.text(movie[m].title));
+      }
+    }
+  });
+});
